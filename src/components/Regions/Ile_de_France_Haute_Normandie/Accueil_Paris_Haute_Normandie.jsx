5bbbea3dc2f8ee949812575c86bfc8a3c631576f
Guillaume Queste <guillaume.queste@laposte.net>
2021-03-31 19:56:17 +0200

    régions ile de france haute normandie

--- src/components/Regions/Ile_de_France_Haute_Normandie/Accueil_Paris_Haute_Normandie.jsx
+++ src/components/Regions/Ile_de_France_Haute_Normandie/Accueil_Paris_Haute_Normandie.jsx
@@ -16,64 +16,151 @@ class Accueil_Paris_Haute_Normandie extends Component {
         <Footer regions={true} />
         <div className="divRegions">
           <div className="liste">
-           <h1 className="titleRegions">accueil</h1>
+           <h1 className="titleRegion">Ile-de-France et Haute-Normandie</h1>
 
-            <Link
-              to="/ile-de-france_haute-normandie"
-              className="linkRegion linkRegionHauteNormandie"
-            >
-              <span className="titleLinkRegion">
-                Ile-de-France & Haute-Normandie
-              </span>
-            </Link>
+            <div className="ligneRegion">
+              <div className="caseRegion">
+                <Link to="/auteuil" className="hippo hippo_auteuil">
+                  <span>Auteuil</span>
+                </Link>
+              </div>
+              <div className="caseRegion">
+                <Link to="/bacqueville" className="hippo hippo_bacqueville">
+                  <span>Bacqueville-en-Caux</span>
+                </Link>
+              </div>
+            </div>
 
-            <Link to="/nord" className="linkRegion linkRegionNord">
-              <span className="titleLinkRegion">Nord</span>
-            </Link>
+            <div className="ligneRegion">
+              <div className="caseRegion">
+                <Link to="/bernay" className="hippo hippo_bernay">
+                  <span>Bernay</span>
+                </Link>
+              </div>
+              <div className="caseRegion">
+              <Link to="/bihorel" className="hippo hippo_bihorel">
+                  <span>Bihorel</span>
+                </Link>
+              </div>
+            </div>
 
-            <Link
-              to="/basse-normandie"
-              className="linkRegion linkRegionBasseNormandie"
-            >
-              <span className="titleLinkListe">Basse-Normandie</span>
-            </Link>
+            <div className="ligneRegion">
+              <div className="caseRegion">
+                <Link to="/chantilly" className="hippo hippo_chantilly">
+                  <span>Chantilly</span>
+                </Link>
+              </div>
+              <div className="caseRegion">
+              <Link to="/chartres" className="hippo hippo_chartres">
+                  <span>Chartres</span>
+                </Link>
+              </div>
+            </div>
 
-            <Link
-              to="/anjou-maine-centre-ouest"
-              className="linkRegion linkRegionAnjouMaineCentreOuest"
-            >
-              <span className="titleLinkRegion">Anjou-Maine-Centre-Ouest</span>
-            </Link>
+            <div className="ligneRegion">
+              <div className="caseRegion">
+                <Link to="/compiegne" className="hippo hippo_compiegne">
+                  <span>Compiègne</span>
+                </Link>
+              </div>
+              <div className="caseRegion">
+              <Link to="/dieppe" className="hippo hippo_dieppe">
+                  <span>Dieppe</span>
+                </Link>
+              </div>
+            </div>
 
-            <Link to="/ouest" className="linkRegion linkRegionOuest">
-              <span className="titleLinkRegion">Ouest</span>
-            </Link>
+            <div className="ligneRegion">
+              <div className="caseRegion">
+                <Link to="/enghien" className="hippo hippo_enghien">
+                  <span>Enghien</span>
+                </Link>
+              </div>
+              <div className="caseRegion">
+              <Link to="/evreux" className="hippo hippo_evreux">
+                  <span>Evreux</span>
+                </Link>
+              </div>
+            </div>
 
-            <Link to="/est" className="linkRegion linkRegionEst">
-              <span className="titleLinkRegion">Est</span>
-            </Link>
+            <div className="ligneRegion">
+              <div className="caseRegion">
+                <Link to="/fontainebleau" className="hippo hippo_fontainebleau">
+                  <span>Fontainebleau</span>
+                </Link>
+              </div>
+              <div className="caseRegion">
+              <Link to="/gournay" className="hippo hippo_gournay">
+                  <span>Gournay en Bray</span>
+                </Link>
+              </div>
+            </div>
 
-            <Link to="/belgique" className="linkRegion linkRegionBelgique">
-              <span className="titleLinkRegion">Belgique</span>
-            </Link>
+            <div className="ligneRegion">
+              <div className="caseRegion">
+                <Link to="/la_ferte-vidame" className="hippo hippo_la_ferte">
+                  <span>La Ferté-Vidame</span>
+                </Link>
+              </div>
+              <div className="caseRegion">
+              <Link to="/le_neubourg" className="hippo hippo_le_neubourg">
+                  <span>Le Neubourg</span>
+                </Link>
+              </div>
+            </div>
 
-            <Link to="/centre-est" className="linkRegion linkRegionCentreEst">
-              <span className="titleLinkRegion">Centre-Est</span>
-            </Link>
+            <div className="ligneRegion">
+              <div className="caseRegion">
+                <Link to="/les_andelys" className="hippo hippo_les_andelys">
+                  <span>Les Andelys</span>
+                </Link>
+              </div>
+              <div className="caseRegion">
+              <Link to="/longchamp" className="hippo hippo_longchamp">
+                  <span>Longchamp</span>
+                </Link>
+              </div>
+            </div>
 
-            <Link to="/sud-est" className="linkRegion linkRegionSudEst">
-              <span className="titleLinkRegion">Sud-Est</span>
-            </Link>
+            <div className="ligneRegion">
+              <div className="caseRegion">
+                <Link to="/maisons_laffitte" className="hippo hippo_maisons">
+                  <span>Maisons-Laffitte</span>
+                </Link>
+              </div>
+              <div className="caseRegion">
+              <Link to="/mauquenchy" className="hippo hippo_mauquenchy">
+                  <span>Mauquenchy</span>
+                </Link>
+              </div>
+            </div>
 
-            <Link to="/sud-ouest" className="linkRegion linkRegionSudOuest">
-              <span className="titleLinkRegion">Sud-Ouest</span>
-            </Link>
+            <div className="ligneRegion">
+              <div className="caseRegion">
+                <Link to="/rambouillet" className="hippo hippo_rambouillet">
+                  <span>Rambouillet</span>
+                </Link>
+              </div>
+              <div className="caseRegion">
+              <Link to="/saint_aubin" className="hippo hippo_saint_aubin">
+                  <span>Saint-Aubin-lès-Elbeuf</span>
+                </Link>
+              </div>
+            </div>
 
-            <Link to="/corse" className="linkRegion linkRegionCorse">
-              <span className="titleLinkRegion">Corse</span>
-            </Link>
-
-            
+            <div className="ligneRegion">
+              <div className="caseRegion">
+                <Link to="/saint-cloud" className="hippo hippo_saint-cloud">
+                  <span>Saint-Cloud</span>
+                </Link>
+              </div>
+              <div className="caseRegion">
+              <Link to="/vincennes" className="hippo hippo_vincennes">
+                  <span>Vincennes</span>
+                </Link>
+              </div>
+            </div>
+ 
           </div>
         </div>
       </div>
